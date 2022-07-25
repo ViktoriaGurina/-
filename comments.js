b2ce5b1dc9109a2a87e6323b4f1ee725fa23d38f
ViktoriaGurina <105868724+ViktoriaGurina@users.noreply.github.com>
2022-07-25 12:42:38 +0300

    Add files via upload

---
+++ comments.js
@@ -0,0 +1,298 @@
+const comments = [
+    {
+        postId: 1,
+        id: 1,
+        name: 'id labore ex et quam loborum',
+        email: 'Seruaqi.com',
+        body: 
+           'laudantium enim quasi est quidem magnam voluptate',
+    },
+    {
+        postId: 1,
+        id: 2,
+        name: 'id labore ex et quam loborum',
+        email: 'Seruaqi.com',
+        body: 
+           'laudantium enim quasi est quidem magnam voluptate',
+    },
+    {
+        postId: 1,
+        id: 3,
+        name: 'id labore ex et quam loborum',
+        email: 'Seruaqi.com',
+        body: 
+           'laudantium enim quasi est quidem magnam voluptate',
+    },
+    {
+        postId: 1,
+        id: 4,
+        name: 'id labore ex et quam loborum',
+        email: 'Seruaqi.com',
+        body: 
+           'laudantium enim quasi est quidem magnam voluptate',
+    },
+    {
+        postId: 1,
+        id: 5,
+        name: 'id labore ex et quam loborum',
+        email: 'Seruaqi.com',
+        body: 
+           'laudantium enim quasi est quidem magnam voluptate',
+    },
+    {
+        postId: 1,
+        id: 6,
+        name: 'id labore ex et quam loborum',
+        email: 'Seruaqi.com',
+        body: 
+           'laudantium enim quasi est quidem magnam voluptate',
+    },
+    {
+        postId: 1,
+        id: 7,
+        name: 'id labore ex et quam loborum',
+        email: 'Seruaqi.com',
+        body: 
+           'laudantium enim quasi est quidem magnam voluptate',
+    },
+    {
+        postId: 1,
+        id: 8,
+        name: 'id labore ex et quam loborum',
+        email: 'Seruaqi.com',
+        body: 
+           'laudantium enim quasi est quidem magnam voluptate',
+    },
+    {
+        postId: 1,
+        id: 9,
+        name: 'id labore ex et quam loborum',
+        email: 'Seruaqi.com',
+        body: 
+           'laudantium enim quasi est quidem magnam voluptate',
+    },
+    {
+        postId: 1,
+        id: 10,
+        name: 'id labore ex et quam loborum',
+        email: 'Seruaqi.com',
+        body: 
+           'laudantium enim quasi est quidem magnam voluptate',
+    },
+    {
+        postId: 1,
+        id: 11,
+        name: 'id labore ex et quam loborum',
+        email: 'Seruaqi.com',
+        body: 
+           'laudantium enim quasi est quidem magnam voluptate',
+    },
+    {
+        postId: 1,
+        id: 12,
+        name: 'id labore ex et quam loborum',
+        email: 'Seruaqi.com',
+        body: 
+           'laudantium enim quasi est quidem magnam voluptate',
+    },
+    {
+        postId: 1,
+        id: 13,
+        name: 'id labore ex et quam loborum',
+        email: 'Seruaqi.com',
+        body: 
+           'laudantium enim quasi est quidem magnam voluptate',
+    },
+    {
+        postId: 1,
+        id: 14,
+        name: 'id labore ex et quam loborum',
+        email: 'Seruaqi.com',
+        body: 
+           'laudantium enim quasi est quidem magnam voluptate',
+    },
+    {
+        postId: 1,
+        id: 15,
+        name: 'id labore ex et quam loborum',
+        email: 'Seruaqi.com',
+        body: 
+           'laudantium enim quasi est quidem magnam voluptate',
+    },
+    {
+        postId: 1,
+        id: 16,
+        name: 'id labore ex et quam loborum',
+        email: 'Seruaqi.com',
+        body: 
+           'laudantium enim quasi est quidem magnam voluptate',
+    },
+    {
+        postId: 1,
+        id: 17,
+        name: 'id labore ex et quam loborum',
+        email: 'Seruaqi.com',
+        body: 
+           'laudantium enim quasi est quidem magnam voluptate',
+    },
+    {
+        postId: 1,
+        id: 18,
+        name: 'id labore ex et quam loborum',
+        email: 'Seruaqi.com',
+        body: 
+           'laudantium enim quasi est quidem magnam voluptate',
+    },
+    {
+        postId: 1,
+        id: 19,
+        name: 'id labore ex et quam loborum',
+        email: 'Seruaqi.com',
+        body: 
+           'laudantium enim quasi est quidem magnam voluptate',
+    },
+    {
+        postId: 1,
+        id: 20,
+        name: 'id labore ex et quam loborum',
+        email: 'Seruaqi.com',
+        body: 
+           'laudantium enim quasi est quidem magnam voluptate',
+    },
+    {
+        postId: 1,
+        id: 21,
+        name: 'id labore ex et quam loborum',
+        email: 'Seruaqi.com',
+        body: 
+           'laudantium enim quasi est quidem magnam voluptate',
+    },
+    {
+        postId: 1,
+        id: 22,
+        name: 'id labore ex et quam loborum',
+        email: 'Seruaqi.com',
+        body: 
+           'laudantium enim quasi est quidem magnam voluptate',
+    },
+    {
+        postId: 1,
+        id: 23,
+        name: 'id labore ex et quam loborum',
+        email: 'Seruaqi.com',
+        body: 
+           'laudantium enim quasi est quidem magnam voluptate',
+    },
+    {
+        postId: 1,
+        id: 24,
+        name: 'id labore ex et quam loborum',
+        email: 'Seruaqi.com',
+        body: 
+           'laudantium enim quasi est quidem magnam voluptate',
+    },
+    {
+        postId: 1,
+        id: 25,
+        name: 'id labore ex et quam loborum',
+        email: 'Seruaqi.com',
+        body: 
+           'laudantium enim quasi est quidem magnam voluptate',
+    },
+    {
+        postId: 1,
+        id: 26,
+        name: 'id labore ex et quam loborum',
+        email: 'Seruaqi.com',
+        body: 
+           'laudantium enim quasi est quidem magnam voluptate',
+    },
+    {
+        postId: 1,
+        id: 27,
+        name: 'id labore ex et quam loborum',
+        email: 'Seruaqi.com',
+        body: 
+           'laudantium enim quasi est quidem magnam voluptate',
+    },
+    {
+        postId: 1,
+        id: 28,
+        name: 'id labore ex et quam loborum',
+        email: 'Seruaqi.com',
+        body: 
+           'laudantium enim quasi est quidem magnam voluptate',
+    },
+    {
+        postId: 1,
+        id: 29,
+        name: 'id labore ex et quam loborum',
+        email: 'Seruaqi.com',
+        body: 
+           'laudantium enim quasi est quidem magnam voluptate',
+    },
+    {
+        postId: 1,
+        id: 30,
+        name: 'id labore ex et quam loborum',
+        email: 'Seruaqi.com',
+        body: 
+           'laudantium enim quasi est quidem magnam voluptate',
+    },
+    {
+        postId: 1,
+        id: 31,
+        name: 'id labore ex et quam loborum',
+        email: 'Seruaqi.com',
+        body: 
+           'laudantium enim quasi est quidem magnam voluptate',
+    },
+    {
+        postId: 1,
+        id: 32,
+        name: 'id labore ex et quam loborum',
+        email: 'Seruaqi.com',
+        body: 
+           'laudantium enim quasi est quidem magnam voluptate',
+    },
+    {
+        postId: 1,
+        id: 33,
+        name: 'id labore ex et quam loborum',
+        email: 'Seruaqi.com',
+        body: 
+           'laudantium enim quasi est quidem magnam voluptate',
+    },
+    {
+        postId: 1,
+        id: 34,
+        name: 'id labore ex et quam loborum',
+        email: 'Seruaqi.com',
+        body: 
+           'laudantium enim quasi est quidem magnam voluptate',
+    },
+    {
+        postId: 1,
+        id: 35,
+        name: 'id labore ex et quam loborum',
+        email: 'Seruaqi.com',
+        body: 
+           'laudantium enim quasi est quidem magnam voluptate',
+    },
+    {
+        postId: 1,
+        id: 36,
+        name: 'id labore ex et quam loborum',
+        email: 'Seruaqi.com',
+        body: 
+           'laudantium enim quasi est quidem magnam voluptate',
+    },
+    {
+        postId: 1,
+        id: 37,
+        name: 'id labore ex et quam loborum',
+        email: 'Seruaqi.com',
+        body: 
+           'laudantium enim quasi est quidem magnam voluptate',
+    },
+]
---
+++ users.js
@@ -0,0 +1,71 @@
+const users = [
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@april.biz',
+        address: {
+            street: 'Kulas Light' , 
+            suite: 'Apt. 556',
+            city: 'Gwenborough',
+            zipcode: '92857-3874',
+            geo: {
+                lat: '-45.7583',
+                lng: '75.-0934',
+            },
+        },
+        phone: '1-2456-87474-43367',
+        website: 'ghdhxhgxjhxj',
+        company: {
+            name: 'Romaguera-Crona',
+            catchPhrase: 'Multi-layered client-server neural-ne',
+            bs: 'harness real-time e-markets',
+        },
+    },
+    {
+        id: 2,
+        name: ' Graham Rknvk',
+        username: 'Yetra',
+        email: 'fkincere@april.biz',
+        address: {
+            street: 'Galas Light' , 
+            suite: 'Apt. 556',
+            city: 'Gwenborough',
+            zipcode: '92857-3874',
+            geo: {
+                lat: '-45.7583',
+                lng: '75.-0934',
+            },
+        },
+        phone: '1-2456-87474-43367',
+        website: 'ghdhxhgxjhxj',
+        company: {
+            name: 'Romaguera-Crona',
+            catchPhrase: 'Multi-layered client-server neural-ne',
+            bs: 'harness real-time e-markets',
+        },
+    },
+    {
+        id: 3,
+    name: 'Leane ujgerm',
+    username: 'Bret',
+    email: 'Sincere@april.biz',
+    address: {
+        street: 'Kulas Light' , 
+        suite: 'Apt. 556',
+        city: 'Gwenborough',
+        zipcode: '92857-3874',
+        geo: {
+            lat: '-45.7583',
+            lng: '75.-0934',
+        },
+    },
+    phone: '1-2456-87474-43367',
+    website: 'ghdhxhgxjhxj',
+    company: {
+        name: 'Romaguera-Crona',
+        catchPhrase: 'Multi-layered client-server neural-ne',
+        bs: 'harness real-time e-markets',
+    },
+    },
+]
